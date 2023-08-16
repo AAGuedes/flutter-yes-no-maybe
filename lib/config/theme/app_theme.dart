e55f11b98ae30dece313b4a325559cd49b077cfc
AAGuedes <airam.a.g.85@gmail.com>
2023-08-16 17:43:37 +0100

    Mis Mensajes - Burbuja de chat

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -22,8 +22,8 @@ class AppTheme {
 
   ThemeData theme() {
     return ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: _colorThemes[selectedColor],
-        brightness: Brightness.dark);
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor],
+    );
   }
 }
